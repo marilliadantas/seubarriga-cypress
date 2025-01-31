ff07290d32513550d6d415ec0ee099715f40430e
Marillia Dantas <marillia.dantas@hotmail.com>
2025-01-31 16:39:14 -0300

    update pipeline

--- cypress/e2e/movement.cy.js
+++ cypress/e2e/movement.cy.js
@@ -18,22 +18,22 @@ describe("Movement Creation", () => {
     });
 
     it("Create pending revenue", () => {
-      movementPage.fillForms("Receita", "04/12/2024","04/12/2024", "Salary", "Nagato", "10.000", "Nubank", "Pendente");
+      movementPage.fillForms("Receita", "31/01/2025","31/01/2025", "Salary", "Nagato", "10.000", "Nubank", "Pendente");
       movementPage.verifyMessage("Movimentação adicionada com sucesso!");
     });
 
     it("Create paid revenue", () => {
-      movementPage.fillForms("Receita", "04/12/2024", "04/12/2024", "Salary", "Nagato", "10.000", "Nubank", "Pago");
+      movementPage.fillForms("Receita", "31/01/2025", "31/01/2025", "Salary", "Nagato", "10.000", "Nubank", "Pago");
       movementPage.verifyMessage("Movimentação adicionada com sucesso!");
     });
 
     it("Create pending expense", () => {
-      movementPage.fillForms("Despesa", "04/12/2024", "04/12/2024", "Salary", "Nagato", "10.000", "Nubank", "Pendente");
+      movementPage.fillForms("Despesa", "31/01/2025", "31/01/2025", "Salary", "Nagato", "10.000", "Nubank", "Pendente");
       movementPage.verifyMessage("Movimentação adicionada com sucesso!");
     });
 
     it("Create paid expense", () => {
-      movementPage.fillForms("Despesa", "04/12/2024", "04/12/2024", "Salary", "Nagato", "10.000", "Nubank", "Pago");
+      movementPage.fillForms("Despesa", "31/01/2025", "31/01/2025", "Salary", "Nagato", "10.000", "Nubank", "Pago");
       movementPage.verifyMessage("Movimentação adicionada com sucesso!");
     });
   });
@@ -49,7 +49,7 @@ describe("Movement Creation", () => {
     });
 
     it("Invalid payment date", () => {
-      movementPage.fillForms("Receita", "04/12/2024", "32/12/2024", "Salary", "Nagato", "10.000", "Nubank", "Pago");
+      movementPage.fillForms("Receita", "31/01/2025", "32/12/2024", "Salary", "Nagato", "10.000", "Nubank", "Pago");
       movementPage.verifyMessage("Data do pagamento inválida (DD/MM/YYYY)");
     });
 
@@ -59,27 +59,27 @@ describe("Movement Creation", () => {
     });
 
     it("Empty payment date field", () => {
-      movementPage.fillForms("Receita", "04/12/2024", "", "Salary", "Nagato", "10.000", "Nubank", "Pago");
+      movementPage.fillForms("Receita", "31/01/2025", "", "Salary", "Nagato", "10.000", "Nubank", "Pago");
       movementPage.verifyMessage("Data do pagamento é obrigatório");
     });
 
     it("Empty description field", () => {
-      movementPage.fillForms("Receita", "04/12/2024", "05/12/2024", "", "Nagato", "10.000", "Nubank", "Pago");
+      movementPage.fillForms("Receita", "31/01/2025", "05/12/2025", "", "Nagato", "10.000", "Nubank", "Pago");
       movementPage.verifyMessage("Descrição é obrigatório");
     });
 
     it("Empty interested field", () => {
-      movementPage.fillForms("Receita", "04/12/2024", "05/12/2024", "Salary", "", "10.000", "Nubank", "Pago");
+      movementPage.fillForms("Receita", "31/01/2025", "05/12/2025", "Salary", "", "10.000", "Nubank", "Pago");
       movementPage.verifyMessage("Interessado é obrigatório");
     });
 
     it("Empty value field", () => {
-      movementPage.fillForms("Receita", "04/12/2024", "05/12/2024", "Salary", "Nagato", "", "Nubank", "Pago");
+      movementPage.fillForms("Receita", "31/01/2025", "05/12/2025", "Salary", "Nagato", "", "Nubank", "Pago");
       movementPage.verifyErrorMessages(["Valor é obrigatório", "Valor deve ser um número"]);
     });
 
     it("Empty value invalid", () => {
-      movementPage.fillForms("Receita", "04/12/2024", "05/12/2024", "Salary", "Nagato", "10,000", "Nubank", "Pago");
+      movementPage.fillForms("Receita", "31/01/2025", "05/12/2025", "Salary", "Nagato", "10,000", "Nubank", "Pago");
       movementPage.verifyMessage("Valor deve ser um número");
     });
   });
